02b218beb4a1e1530961adbf798597a539f4ccd9
Marco Cesarato <marco.cesarato@eniac.it>
2021-02-19 15:21:52 +0100

    feat(Utils): add color contrast function

--- src/Utils.js
+++ src/Utils.js
@@ -89,3 +89,24 @@ export const mergeViewStyle = (style, defaultStyle) => {
 	}
 	return style;
 };
+
+/**
+ * Get color contrast
+ * @param hexcolor
+ * @returns {string}
+ */
+export const getColorContrast = (hexcolor) => {
+	if (hexcolor.slice(0, 1) === '#') {
+		hexcolor = hexcolor.slice(1);
+	}
+	if (hexcolor.length === 3) {
+		hexcolor = hexcolor.split('').map(function (hex) {
+			return hex + hex;
+		}).join('');
+	}
+	var r = parseInt(hexcolor.substr(0,2),16);
+	var g = parseInt(hexcolor.substr(2,2),16);
+	var b = parseInt(hexcolor.substr(4,2),16);
+	var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+	return (yiq >= 128) ? '#000000' : '#FFFFFF';
+};
