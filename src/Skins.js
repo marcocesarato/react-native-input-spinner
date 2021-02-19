ccd8ed01d13e54e68b416d8108d6d759753c1b95
Marco Cesarato <cesarato.developer@gmail.com>
2021-02-19 20:52:49 +0100

    refactor(Skin): split skin logic to new file

---
+++ src/Skins.js
@@ -0,0 +1,26 @@
+import * as CleanSkin from "./skins/CleanSkin";
+import * as ModernSkin from "./skins/ModernSkin";
+import * as PaperSkin from "./skins/PaperSkin";
+import * as RoundSkin from "./skins/RoundSkin";
+import * as SquareSkin from "./skins/SquareSkin";
+
+/**
+ * Get skin props
+ * @returns {*}
+ */
+export const getSkin = (skin, props) => {
+	switch (skin) {
+		case "clean":
+			return CleanSkin.getProps(props);
+		case "modern":
+			return ModernSkin.getProps(props);
+		case "paper":
+			return PaperSkin.getProps(props);
+		case "round":
+			return RoundSkin.getProps(props);
+		case "square":
+			return SquareSkin.getProps(props);
+		default:
+			return {};
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import React from "react";
 import InputSpinner from "./InputSpinner";
-
-// Skins
-import * as CleanSkin from "./skins/CleanSkin";
-import * as ModernSkin from "./skins/ModernSkin";
-import * as PaperSkin from "./skins/PaperSkin";
-import * as RoundSkin from "./skins/RoundSkin";
-import * as SquareSkin from "./skins/SquareSkin";
+import {getSkin} from "./Skins";
 
 // Export
 export default (props) => {
 	const skin = String(props.skin).trim().toLowerCase();
-	let skinProps;
-	switch (skin) {
-		case "clean":
-			skinProps = CleanSkin.getProps(props);
-			break;
-		case "modern":
-			skinProps = ModernSkin.getProps(props);
-			break;
-		case "paper":
-			skinProps = PaperSkin.getProps(props);
-			break;
-		case "round":
-			skinProps = RoundSkin.getProps(props);
-			break;
-		case "square":
-			skinProps = SquareSkin.getProps(props);
-			break;
-		default:
-			skinProps = {};
-	}
-	let overwriteProps = {...props, ...skinProps};
+	const skinProps = getSkin(skin, props);
+	const overwriteProps = {...props, ...skinProps};
 	return <InputSpinner {...overwriteProps} />;
 };
